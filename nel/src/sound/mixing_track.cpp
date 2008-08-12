add890a67be8e11dee779f87d7bd3a56f2ea636b
vl <vl@60ea55b9-4480-4529-b8ac-ed5291525b0f>
2008-08-12 14:01:20 +0000

    git-svn-id: http://dev.ryzom.com/svn/trunk@1 60ea55b9-4480-4529-b8ac-ed5291525b0f

---
+++ nel/src/sound/mixing_track.cpp
@@ -0,0 +1,39 @@
+/** \file mixing_track.cpp
+ * CTrack: a source selected for playing
+ *
+ * $Id: mixing_track.cpp,v 1.5 2004/10/29 18:08:47 berenguier Exp $
+ */
+
+/* Copyright, 2001 Nevrax Ltd.
+ *
+ * This file is part of NEVRAX NEL.
+ * NEVRAX NEL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+
+ * NEVRAX NEL is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with NEVRAX NEL; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+#include "stdsound.h"
+
+#include "mixing_track.h"
+
+
+// leave not static else this workaround don't work
+void	dummyToAvoidStupidCompilerWarning_sound_mixing_track_cpp()
+{
+}
+
+namespace NLSOUND {
+
+
+} // NLSOUND
