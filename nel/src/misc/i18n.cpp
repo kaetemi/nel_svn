02bec090813ae0d5f9f6ff2e5259a8cb842409d4
vl <vl@60ea55b9-4480-4529-b8ac-ed5291525b0f>
2009-03-06 16:52:27 +0000

    Fixed: we must *not* remove right white spaces in the text or the formating will not work
    git-svn-id: http://dev.ryzom.com/svn/trunk@1166 60ea55b9-4480-4529-b8ac-ed5291525b0f

--- nel/src/misc/i18n.cpp
+++ nel/src/misc/i18n.cpp
@@ -93,9 +93,6 @@ bool CI18N::loadFileIntoMap(const string &fileName, StrMapContainer &destMap)
 			return false;
 		}
 
-		// remove white spaces that can be in the end of the string
-		ucs = trimRightWhiteSpaces(ucs);
-
 		// ok, a line read.
 		pair<map<string, ucstring>::iterator, bool> ret;
 		ret = destMap.insert(make_pair(label, ucs));
