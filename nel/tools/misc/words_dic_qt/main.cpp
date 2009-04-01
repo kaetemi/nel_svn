ec9d012ee7616ef77eab9391a2b150054ca30c8b
sfb <sfb@60ea55b9-4480-4529-b8ac-ed5291525b0f>
2009-04-01 17:12:14 +0000

    Changed: words_dic_qt searches $HOME/.nel and $CMAKE_INSTALL_PREFIX/share/nel/words_dic in addition to CWD for its config.
    git-svn-id: http://dev.ryzom.com/svn/trunk@1247 60ea55b9-4480-4529-b8ac-ed5291525b0f

--- nel/tools/misc/words_dic_qt/main.cpp
+++ nel/tools/misc/words_dic_qt/main.cpp
@@ -1,7 +1,15 @@
 #include <QApplication>
 #include "nel/misc/app_context.h"
+#include "nel/misc/path.h"
 #include "words_dicDlg.h"
 
+#ifdef NL_OS_UNIX
+#include <stdlib.h>
+#endif // NL_OS_UNIX
+
+#ifndef NL_WORDS_DIC_CFG
+#define NL_WORDS_DIC_CFG ""
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +19,13 @@ int main(int argc, char *argv[])
 	// of your final executable, you should remove from the following
 	// the specific initialization routines you do not need.
 	NLMISC::CApplicationContext myApplicationContext;
+
+	NLMISC::CPath::addSearchPath(NL_WORDS_DIC_CFG);
+
+#ifdef NL_OS_UNIX
+	std::string homeDir = getenv("HOME");
+	NLMISC::CPath::addSearchPath( homeDir + "/.nel");
+#endif // NL_OS_UNIX
 
 	Q_INIT_RESOURCE(words_dic_Qt);
 	QApplication app(argc, argv);
